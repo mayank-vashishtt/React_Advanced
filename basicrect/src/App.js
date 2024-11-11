fd7ccff2a5b2d6a5fbbddfc2d168a1d7e8606235
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-11-11 23:02:38 +0530

    added counter app and notes

---
+++ basicrect/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h2>Basic React</h2>
+  );
+}
+
+export default App;
---
+++ basicvite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>vite react app </h1>
+    <h1>vite react app </h1>
+    </>
+    
+  )
+}
+
+export default App
